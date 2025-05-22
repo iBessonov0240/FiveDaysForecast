aaf4c5481a1328556b1571380c7f2381492c1137
Ilia <ilya2491@gmail.com>
2025-05-22 16:28:19 +0700

    Create project.

---
+++ FiveDaysForecast/FiveDaysForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiveDaysForecastApp.swift
+//  FiveDaysForecast
+//
+//  Created by i0240 on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FiveDaysForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
